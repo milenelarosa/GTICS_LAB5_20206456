33a58f21f63c5965cdd518396658b0586a402227
milenelarosa <a20206456@pucp.edu.pe>
2024-05-08 19:17:38 -0500

    Subiendo proyecto

---
+++ lab5_20206456/src/main/java/com/example/lab5_20206456/Lab520206456Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20206456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520206456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520206456Application.class, args);
+	}
+
+}
